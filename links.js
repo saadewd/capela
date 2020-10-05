10fbb1ff2063cb653dddd86ee7df07f74e30d809
TBDev3 <tb_dev3@thunderbees.com>
2020-10-05 18:33:38 +0500

    6:33

---
+++ links.js
@@ -0,0 +1,180 @@
+let host = 'http://'+window.location.host;
+let santoData = {
+
+    August: {
+      04: {
+        img: host+'/img/04_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'São João Maria Vianney',
+        details: "Hoje lembramos o Santo Cura D’Ars, o patrono dos párocos, que ficou conhecido em toda França pe contribuição que deu às pessoas vindas de perto e de longe para a confissão. Celebramos o Dia doPadre",
+        date: '04/08/2020',
+        route: host+'/Dia-4.html'
+      },
+      06: {
+        img: host+'/img/06_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Transfiguração do Senhor',
+        details: 'A transfiguração de Jesus foi uma experiência extraordinária que reforçou a fé dos discípulos em Cristo. Hoje também somos chamados a nos transformar em pessoas melhores e mais solidárias, seguindo o exemplo de Jesus',
+        date: '06/08/2020',
+        route: host+'/Dia-6.html'
+      },
+      09: {
+        img: host+'/img/09_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Santa Teresa Benedita da Cruz',
+        details: 'Hoje a Igreja celebra Santa Teresa Benedita da Cruz (Edith Stein), grande filósofa e mística que soube buscar sempre a verdade e o amor. Em sua rica vida, temos uma síntese do drama humano que ainda necessita ser sarado. Que ela rogue por nós!',
+        date: '9/08/2020',
+        route: host+'/Dia-9.html'
+      },
+      11: {
+        img: host+'/img/11_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Santa Clara de Assis',
+        details: 'Santa Clara nos inspira a ter uma vida de despojamento dos bens materiais, seguindo o Cristo pobre. Junto com São Francisco, fundou a ordem das clarissas e é a patrona da televisão por causa de uma visão que teve. ',
+        date: '11/08/2020',
+        route: host+'/Dia-11.html'
+      },
+      14: {
+        img: host+'/img/14_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'São Maximiliano Maria Kolbe',
+        details: 'São Maximiliano é um santo atual que deu a vida num campo de concentração nazista. Ele foi capaz de sensibilizar-se com o sofrimento do outro, oferecendo a própria vida para morrer no lugar de um pai de família.',
+        date: '14/08/2020',
+        route: host+'/Dia-14.html'
+      },
+      16: {
+        img: host+'/img/16_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Assunção de Maria',
+        details: 'Hoje a Igreja celebra a Solenidade da Assunção de Maria. A Assunção é a participação de Maria na ressurreição de Cristo. Assim, é uma antecipação da nossa ressurreição. Que Maria nos ajude a lutar pelo bem, pela paz e pela justiça.',
+        date: '16/08/2020',
+        route: host+'/Dia-16.html'
+      },
+      22: {
+        img: host+'/img/22_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Nossa Senhora Rainha',
+        details: 'Celebramos hoje a memória de Nossa Senhora Rainha. A festividade foi instituída pelo Papa Pio XII, em 1954, e nada tem a ver com poder, mas com a realeza do serviço.',
+        date: '22/08/2020',
+        route: host+'/Dia-22.html'
+      },
+      23: {
+        img: host+'/img/23_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Santa Rosa de Lima',
+        details: 'HSanta Rosa de Lima viveu a vida religiosa num pequeno quarto, no fundo do quintal de casa, em oração, penitência e cuidado com os doentes, sobretudo indígenas e negros. Era uma mística que viveu a experiência do amor de Deus em sua vida.[..]',
+        date: '23/08/2020',
+        route: host+'/Dia-23.html'
+      },
+      24: {
+        img: host+'/img/24_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'São Bartolomeu',
+        details: 'Celebramos hoje a festa de São Bartolomeu, um dos primeiros apóstolos de Jesus. No diálogo com Filipe, reconhece o Mestre que vem a seu encontro, e o segue. Deu sua vida ao anúncio do Evangelho, testemunhando o Cristo Ressuscitado [..].',
+        date: '24/08/2020',
+        route: host+'/Dia-24.html'
+      },
+      27: {
+        img: host+'/img/27_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Santa Mônica',
+        details: 'Celebramos hoje a memória de Santa Mônica, mãe de Santo Agostinho, por cuja conversão derramou muitas lágrimas. Mas Deus, consolação dos que choram, escutou seus gemidos e veio em seu socorro, cobrindo-a de sua misericórdia[..].',
+        date: '27/08/2020',
+        route: host+'/Dia-27.html'
+      },
+      28: {
+        img: host+'/img/28_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Santo Agostinho',
+        details: 'Santo Agostinho, de quem fazemos memória hoje, nos inspire à conversão, acolhendo a graça de Deus em nossa vida e buscando crescer na fé por meio da Palavra de Deus[..].',
+        date: '28/08/2020',
+        route: host+'/Dia-28.html'
+      },
+      29: {
+        img: host+'/img/29_Post_Dia_Santo_Agosto_2020.jpg',
+        title: 'Martírio de São João Batista',
+        details: 'Hoje a Igreja celebra o martírio de São João Batista. Esta festa tem suas origens por volta dos séculos V e VI, e está vinculada à dedicação da igreja construída em Sebaste, na Samaria, no suposto túmulo do Precursor de Cristo[..].',
+        date: '29/08/2020',
+        route: host+'/Dia-29.html'
+      },
+    },
+  
+    September: {
+      3: {
+        img: host+'/img/03_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Gregório Magno',
+        details: 'Celebrando hoje São Gregório Magno, reconhecemos a grande contribuição desse Papa à Igreja, como guia de seu rebanho. Que, de sua fidelidade, aprendamos a fé e o amor a Deus por nosso compromisso com os mais sofridos. São Gregório, rogai por nós!',
+        date: '03/09/2020',
+        route: host+'/Dia-September/Dia-3.html'
+      },
+      5: {
+        img: host+'/img/05_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'Santa Teresa de Calcutá',
+        details: ' Hoje a Igreja celebra a memória de Santa Teresa de Calcutá. Seguindo a vontade de Deus, dedicou toda sua vida a amar sobretudo os pobres. “Temos de ir à procura das pessoas, porque podem ter fome de pão ou de amizade”, costumava dizer. ',
+        date: '05/09/2020',
+        route: host+'/Dia-September/Dia-5.html'
+      },
+      15: {
+        img: host+'/img/15_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'Nossa Senhora das Dores',
+        details: 'Nossa Senhora das Dores, de quem fazemos memória hoje, interceda por nós, seus filhos, neste mundo tão dilacerando pelo sofrimento pelo qual passam milhões de pessoas.',
+        date: '15/09/2020',
+        route: host+'/Dia-September/Dia-15.html'
+      },
+      16: {
+        img: host+'/img/16_Post_Dia_Santo_Setembro_2020.jpg',
+        title: ' São Cornélio e São Cipriano',
+        details: 'O Papa Cornélio e o bispo de Cartago, Cipriano, cujas memórias celebramos hoje, foram vítimas ilustres da perseguição.',
+        date: '16/09/2020',
+        route: host+'/Dia-September/Dia-16.html'
+      },
+      19: {
+        img: host+'/img/19_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'Assunção de Maria',
+        details: 'Hoje a Igreja celebra a Solenidade da Assunção de Maria. A Assunção é a participação de Maria na ressurreição de Cristo. Assim, é uma antecipação da nossa ressurreição. Que Maria nos ajude a lutarpelo bem, pela paz e pela justiça..',
+        date: '19/09/2020',
+        route: host+'/Dia-September/Dia-19.html'
+      },
+      21: {
+        img: host+'/img/21_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Mateus Apóstolo e Evangelista',
+        details: 'Que São Mateus, um dos doze apóstolos de Jesus, nos inspire a estar atentos ao chamado do Senhor, a nos colocar a caminho e de coração aberto para a missão.',
+        date: '21/09/2020',
+        route: host+'/Dia-September/Dia-21.html'
+      },
+      23: {
+        img: host+'/img/23_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Pio de Pietrelcina',
+        details: 'Hoje é dia de São Pio de Pietrelcina, o Padre Pio. Ele é um dos santos que mais se assemelharam a Nosso Senhor Jesus Cristo. Marcado pelo profundo amor a Deus e às pessoas, dizia: “Ficarei na porta do Paraíso até o último de meus filhos entrar',
+        date: '23/09/2020',
+        route: host+'/Dia-September/Dia-23.html'
+      },
+      26: {
+        img: host+'/img/26_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Cosme e Damião, Mártires',
+        details: ' Cosme e Damião foram escolhidos, por sua profissão, como patronos dos médicos e dos farmacêuticos. Sabe-se pouco da vida e do martírio desses dois santos, e as lendas não demoraram, colorindo de fantasias sua biografia.',
+        date: '26/09/2020',
+        route: host+'/Dia-September/Dia-26.html'
+      },
+      27: {
+        img: host+'/img/27_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Vicente de Paulo',
+        details: '    Vicente de Paulo nasceu em Pouy, na França, em 24 de abril de 1581. Filho de pobres camponeses, gostava de estudar e se destacava por sua inteligência e zelo religioso. ',
+        date: '27/09/2020',
+        route: host+'/Dia-September/Dia-27.html'
+      },
+  
+      29: {
+        img: host+'/img/29_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Miguel, São Gabriel e São Rafael, Arcanjos',
+        details: 'Os arcanjos Miguel, Gabriel e Rafael são seres celestiais que estão a serviço de Deus como mensageiros e protetores.',
+        date: '29/09/2020',
+        route: host+'/Dia-September/Dia-29.html'
+      },
+      30: {
+        img: host+'/img/30_Post_Dia_Santo_Setembro_2020.jpg',
+        title: 'São Jerônimo',
+        details: 'São Jerônimo foi um escritor de estilo rico, capaz de pensar e escrever em latim, grego e hebraico. Nele se revela o filósofo, o retórico, o gramático e o dialético.',
+        date: '30/09/2020',
+        route: host+'/Dia-September/Dia-30.html'
+      }
+    }
+}
+    
+console.log(santoData)
+var date = new Date();
+let currentDate = date.getDate();
+
+console.log(santoData.September[3])
+// if(santoData.September[3])
+
+  
