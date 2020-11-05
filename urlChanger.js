fe9cf75e6052a2ced556bfee34fc734d4209a6f2
TBDev2 <tb_dev2@thunderbees.com>
2020-11-05 16:38:01 +0500

    4:37

--- urlChanger.js
+++ urlChanger.js
@@ -1,6 +1,16 @@
 
-if (window.innerWidth <= 700) {
+if (window.innerWidth <= 700 && navigator.userAgent.indexOf("Android")||navigator.userAgent.indexOf("iOS")) {
     console.log('----------------------')
+    let u ='capelaMobile'
+    let lo = u.concat(window.location.pathname)
+
+
+        // window.location.replace("www.facebook.com");
+    
+    window.location.replace(`http://capelamissionaria.org.br/${lo}`);
     // window.location.origin = "http://ewdtech.com/capelaMobile/";
-    window.location.replace("http://ewdtech.com/capelaMobile");
 }
+let u ='capelaMobile'
+let lo = u.concat(window.location)
+// let lo = window.location
+console.log("Location  ::",lo)
